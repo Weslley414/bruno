df7bc6afcecad536775843a17579191944a8eadb
Weslley414 <162639845+Weslley414@users.noreply.github.com>
2025-09-04 20:22:41 -0300

    Add files via upload
    MVC

---
+++ api-rest/server.js
@@ -0,0 +1,25 @@
+import app from './src/app/app.js'
+
+import conexao from './src/app/database/conexao.js'
+
+const port = 3000
+
+// //  Estabelecer a conexao
+// conexao.connect((error) => {
+//     if (error) {
+//         console.log('Erro ao conectar com o banco de dados:', error)
+//         return
+//     } else {
+//         console.log('Conexão com o banco de dados estabelecida com sucesso!')
+//     }
+// })
+
+// // Listening (Escutando)
+// app.listen(port, () => {
+//     console.log(`servidor rodando em http://localhost:${port}`)
+// })
+
+// Listening (Escutando)
+app.listen(port, () => {
+    console.log(`servidor rodando em http://localhost:${port}`)
+})
